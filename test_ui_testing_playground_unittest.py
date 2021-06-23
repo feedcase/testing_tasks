7d69280be015fc880dac1140e3511393110c93bb
feedcase <seva1502@gmail.com>
2021-06-23 15:14:59 +0300

    Initial commit

--- test_ui_testing_playground_unittest.py
+++ test_ui_testing_playground_unittest.py
@@ -10,19 +10,19 @@ class TestPageUI(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
 
-        capabilities = {
-            "browserName": "chrome",
-            "browserVersion": "90.0",
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": False
-            }
-        }
-
-        driver = webdriver.Remote(
-            command_executor="http://localhost:4444/wd/hub",
-            desired_capabilities=capabilities)
-        # driver = webdriver.Chrome()
+        # capabilities = {
+        #     "browserName": "chrome",
+        #     "browserVersion": "90.0",
+        #     "selenoid:options": {
+        #         "enableVNC": True,
+        #         "enableVideo": False
+        #     }
+        # }
+        #
+        # driver = webdriver.Remote(
+        #     command_executor="http://localhost:4444/wd/hub",
+        #     desired_capabilities=capabilities)
+        driver = webdriver.Chrome()
         driver.maximize_window()
         cls.driver = driver
 
